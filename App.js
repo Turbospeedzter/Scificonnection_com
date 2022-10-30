b37602f6749743f4690a8395e76f0329f66ed55c
Ted Bailey <tedrbailey@gmail.com>
2022-10-30 16:11:59 -0500

    initial commit
    Add first files

---
+++ App.js
@@ -0,0 +1,12 @@
+//import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ScifiConnection</h1>
+    </div>
+  );
+}
+
+export default App;
